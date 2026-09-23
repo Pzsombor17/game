ba9407dfe3594794acfdc693fa471d8d870d44f1
Pzsombor17 <Pap.Zsombor@C19-01>
2026-09-23 11:11:37 +0200

    Add project files.

---
+++ game/game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game
+{
+    public class game
+    {
+        public string name {get; set;}
+        public string genre {get; set;}
+        public string Publisher {get; set;}
+        public int year {get; set;}
+        public int price {get; set;}
+        public double rating {get; set;}
+
+        public game (string line)
+        {
+            string[] temp = line.Split(";");
+            name = temp[0];
+            genre = temp[1];
+            Publisher = temp[2];
+            year = int.Parse(temp[3]);
+            price = int.Parse(temp[4]);
+            rating = Convert.ToDouble(temp[5].Replace('.',','));
+
+
+        }
+    }
+}
---
+++ game/model.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game
+{
+    public class model
+    {
+        public List<game> games = new();
+        private void Import()
+        {
+            games =File.ReadAllLines("games.txt").Select(x=> new game(x)).ToList();
+
+        }
+        public model()
+        {
+            Import();
+        }
+        public Dictionary<string,int> PublisherCount()
+        {
+            return games.GroupBy(x => x.Publisher).ToDictionary(x => x.Key, x => x.Count());
+        }
+
+    }
+}
